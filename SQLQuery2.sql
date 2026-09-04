c5e56457144a1b81bd82f8e8d631c18ff5b5bdc9
Lusani-No1 <ST10474461@rcconnect.edu.za>
2026-09-04 13:44:58 +0200

    Are they there

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -5,3 +5,16 @@ BEGIN
 	CREATE DATABASE RaceDayDB;
 END
 GO
+
+USE RaceDayDB;
+GO
+
+/* Drop tables if they already exist (clean re-run support) Order matters because of foreign key dependencies. */
+IF OBJECT_ID('dbo.Results', 'U') IS NOT NULL DROP TABLE dbo.Results;
+IF OBJECT_ID('dbo.Enrolments', 'U') IS NOT NULL DROP TABLE dbo.Enrolments;
+IF OBJECT_ID('dbo.Categories', 'U') IS NOT NULL DROP TABLE dbo.Categories;
+IF OBJECT_ID('dbo.Events', 'U') IS NOT NULL DROP TABLE dbo.Events;
+IF OBJECT_ID('dbo.UserProfiles', 'U') IS NOT NULL DROP TABLE dbo.UserProfiles;
+IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL DROP TABLE dbo.Users;
+GO
+
